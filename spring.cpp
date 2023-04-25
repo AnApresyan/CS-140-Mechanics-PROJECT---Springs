8526b6122467f13286e78ad916610cc4b048ae4d
Anahit Apresyan <anyut@Anahits-MacBook-Air.local>
2023-04-25 17:47:21 +0400

    Task 1 done:D

--- spring.cpp
+++ spring.cpp
@@ -16,20 +16,41 @@ void spring::setK(double k)
 
 std::vector<double> spring::move(double t, double dt, double x0, double v0)
 {
+    std::vector<double> coords;
+    double omega = sqrt(_k);
 
+    for (double time = 0; time <= t; time+=dt)
+        coords.push_back(x0 * cos(omega * time) + v0/omega * sin(omega * time));
+    return (coords);
 }
 
 std::vector<double> spring::move(double t, double dt, double x0)
 {
+    std::vector<double> coords;
+    double omega = sqrt(_k);
 
+    for (double time = 0; time <= t; time+=dt)
+        coords.push_back(x0 * cos(omega * time));
+    return (coords);
 }
 
 std::vector<double> spring::move(double t0, double t1, double dt, double x0, double v0)
 {
+    std::vector<double> coords;
+    double omega = sqrt(_k);
 
+    for (double time = t0; time <= t1; time+=dt)
+        coords.push_back(x0 * cos(omega * time) + v0/omega * sin(omega * time));
+    return (coords);
 }
+
 std::vector<double> spring::move(double t0, double t1, double dt, double x0, double v0, double m)
 {
-    
+    std::vector<double> coords;
+    double omega = sqrt(_k/m);
+
+    for (double time = t0; time <= t1; time+=dt)
+        coords.push_back(x0 * cos(omega * time) + v0/omega * sin(omega * time));
+    return (coords);
 }
 
